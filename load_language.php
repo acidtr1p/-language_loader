2adef2433ac0eaebeb09eabb2d24d17930088bd5
acidtr1p <mrxdoes@gmail.com>
2022-02-27 23:53:55 +1100

    Create load_language.php

---
+++ load_language.php
@@ -0,0 +1,23 @@
+<?php
+function load_language($mod_language_name)
+{
+    // control if input language exist in current user's language path, else return default
+    global $THIS_BASEPATH, $USERLANG, $language;
+
+    if (@file_exists($USERLANG.'/'.$mod_language_name)) {
+        if ($USERLANG != $THIS_BASEPATH.'/language/english') {
+            include_once $THIS_BASEPATH.'/language/english/'.$mod_language_name;
+        }
+
+        return $USERLANG.'/'.$mod_language_name;
+    }
+
+    return $THIS_BASEPATH.'/language/english/'.$mod_language_name;
+}
+?>
+
+#How 2 Use:
+<?php
+global $USERLANG;
+require_once load_language('lang_blocks.php');
+?>
